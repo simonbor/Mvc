08fb30af4cee1fe9bfa2292d316fe14809e357b9
ryanbrandenburg <rybrande@microsoft.com>
2015-10-20 09:11:42 -0700

    * Add tests

---
+++ samples/MvcSubAreaSample.Web/Tests/TestSubAreaAddressing.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using MvcSubAreaSample.Web;
+using Microsoft.AspNet.Mvc.Abstractions;
+using Microsoft.AspNet.Mvc.Routing;
+
+namespace MvcSubAreaSample.Web.Tests
+{
+    public class TestSubAreaAddressing
+    {
+        [Fact]
+        public async Task ExecuteResultAsync_SubAreaGivesCorrectRoute()
+        {
+            var actionDescriptor = CreateActionDescriptor("Menu", "Restaurant", "Home", "Index");
+
+            throw new NotImplementedException();
+        }
+
+        private static ActionDescriptor CreateActionDescriptor(string subArea, string area, string controller, string action)
+        {
+            var actionDescriptor = new ActionDescriptor()
+            {
+                Name = $"SubArea: {subArea}, Area: {area}, Controller: {controller}, Action: {action}",
+                RouteConstraints = new List<RouteDataActionConstraint>()
+            };
+
+            actionDescriptor.RouteConstraints.Add(
+                area == null ?
+                new RouteDataActionConstraint("area", null) :
+                new RouteDataActionConstraint("area", area));
+
+            actionDescriptor.RouteConstraints.Add(
+                subArea == null ?
+                new RouteDataActionConstraint("subarea", null) :
+                new RouteDataActionConstraint("subarea", subArea));
+
+            actionDescriptor.RouteConstraints.Add(
+                controller == null ?
+                new RouteDataActionConstraint("controller", null) :
+                new RouteDataActionConstraint("controller", controller));
+
+            actionDescriptor.RouteConstraints.Add(
+                action == null ?
+                new RouteDataActionConstraint("action", null) :
+                new RouteDataActionConstraint("action", action));
+
+            return actionDescriptor;
+        }
+    }
+}
